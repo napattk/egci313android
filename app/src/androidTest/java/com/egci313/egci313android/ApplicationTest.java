f70008a3a8b8ccf4f0bfd9d7af35313e43a0e182
6272 <napatt.kuttikun@gmail.com>
2016-11-07 13:35:58 +0700

    initial code

---
+++ app/src/androidTest/java/com/egci313/egci313android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.egci313.egci313android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
